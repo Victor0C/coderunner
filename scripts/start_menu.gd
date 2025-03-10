0f3e63aa41e9431207d01b7240097ca971b73df0
Victor Hugo <hugovco511@gmail.com>
2025-03-10 20:04:25 -0300

    fix: start

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://fases/main.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 
 func _on_quit_pressed():
